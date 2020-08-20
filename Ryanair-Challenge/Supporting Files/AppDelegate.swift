ce886036a044f8648c3c26a331ef20e0f2157100
Peter Molnar <peter@petermolnar.hu>
2020-08-20 22:56:31 +0200

    Initial project setup

---
+++ Ryanair-Challenge/Supporting Files/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  Ryanair-Challenge
+//
+//  Created by Peter Molnar on 20/08/2020.
+//  Copyright © 2020 Peter Molnar. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+
+}
+
