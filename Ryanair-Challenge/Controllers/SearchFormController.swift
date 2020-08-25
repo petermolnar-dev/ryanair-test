7d434dc908693db914f5a3d7b206df43bc8efc0e
Peter Molnar <peter.molnar@vodafone.com>
2020-08-25 22:13:48 +0200

    Basic skeleton of the Search View controller is ready

---
+++ Ryanair-Challenge/Controllers/SearchFormController.swift
@@ -0,0 +1,24 @@
+//
+//  SearchFormController.swift
+//  Ryanair-Challenge
+//
+//  Created by Molnar, Peter, Vodafone Malta on 25/08/2020.
+//  Copyright © 2020 Peter Molnar. All rights reserved.
+//
+import Foundation
+import UIKit
+
+class SearchFormController: UIViewController {
+    @IBOutlet var origin: UITextField!
+    @IBOutlet var destination: UITextField!
+    @IBOutlet var adultsCnt: UITextField!
+    @IBOutlet var teenCnt: UITextField!
+    @IBOutlet var childCnt: UITextField!
+    @IBOutlet var departureDate: UITextField!
+    
+    
+
+    @IBAction func tapSearch(_ sender: UIButton) {
+    }
+    
+}
